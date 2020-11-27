193665b97b6ddae7568aba2277118989e6ffad25
Alexgta <9720560@gmail.com>
2020-11-26 19:25:17 -0500

    0. Что такое атомарность? #6856

---
+++ l3_p010_Multithreading/src/main/java/ru/job4j/sharedResources/Cache.java
@@ -0,0 +1,16 @@
+package ru.job4j.sharedResources;
+
+public class Cache {
+
+    private static Cache cache;
+
+    private static Cache instOf() {
+        if (cache == null) {
+            cache = new Cache();
+        }
+        // Reading and assigning a value occurs in 3 steps (reading, initialization, assignment).
+        // So this is not an atomic value.
+        return cache;
+    }
+
+}
