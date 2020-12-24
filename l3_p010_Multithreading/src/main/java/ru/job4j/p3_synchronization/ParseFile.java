1cbeadafbfda1ac3d5a95e62acffe390c7bd1123
Alexgta <9720560@gmail.com>
2020-12-23 21:43:12 -0500

    Visibility. #1102-3. Return 2

--- l3_p010_Multithreading/src/main/java/ru/job4j/p3_synchronization/ParseFile.java
+++ l3_p010_Multithreading/src/main/java/ru/job4j/p3_synchronization/ParseFile.java
@@ -14,6 +14,28 @@ public class ParseFile {
         return file;
     }
 
+    private String getContentFromParam(int paramValue) throws IOException {
+        File currentFile = getFile();
+        InputStream i = new FileInputStream(file);
+        String output = "";
+        int data;
+        while ((data = i.read()) > 0) {
+
+            if (data < paramValue) {
+                output += (char) data;
+            }
+        }
+        return output;
+    }
+
+    public String getContent() throws IOException {
+        return getContentFromParam(Integer.MAX_VALUE);
+    }
+
+    public String getContentWithoutUnicode() throws IOException {
+        return getContentFromParam(0x80);
+    }
+
     public synchronized void saveContent(String content) throws IOException {
         OutputStream o = new FileOutputStream(file, true);  // to append content, not overwrite it
         for (int i = 0; i < content.length(); i += 1) {
